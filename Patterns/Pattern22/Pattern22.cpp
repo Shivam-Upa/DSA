a6509512004b39416fe84c0d5478db0f4278cbea
Shivam Upadhyay <70527646+Shivam-Upa@users.noreply.github.com>
2022-09-25 13:27:16 +0530

    Add files via upload

---
+++ Patterns/Pattern22/Pattern22.cpp
@@ -0,0 +1,34 @@
+//    1
+//   22
+//  333
+// 4444
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    int row = 1;
+
+    while (row <= n)
+    {
+        int space = n - row;
+        while (space)
+        {
+            cout << " ";
+            space -= 1;
+        }
+        int col = 1;
+        while (col <= row)
+        {
+            cout << row;
+            col += 1;
+        }
+
+        cout << endl;
+        row += 1;
+    }
+}
