1781d22699d615c4d2a22e44ad16b2c13c98bb07
Shivam Upadhyay <70527646+Shivam-Upa@users.noreply.github.com>
2023-04-01 12:49:14 +0530

    Add files via upload
    patterns folder

---
+++ Pattern in CPP/Pattern00.cpp
@@ -0,0 +1,38 @@
+// ****
+// ****
+// ****
+// ****
+
+
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+
+    //using for
+    // for (int  i = 0; i < n; i++)
+    // {
+    //     for (int j = 0; j < n; j++)
+    //     {
+    //         cout<<"*";
+    //     }
+    //     cout<<endl;
+        
+    // }
+    
+    //using while
+    int i=1;
+    while (i<=n)
+    {
+        int j=1;
+        while (j<=n)
+        {
+            cout<<"*";
+            j++;
+        }
+        cout<<endl;
+        i++;
+    }
+    
+}
