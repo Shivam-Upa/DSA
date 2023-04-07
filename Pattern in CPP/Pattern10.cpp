4abc05a6f5c6a08c3e1a904b4b9136f0cefc74bb
Shivam Upadhyay <70527646+Shivam-Upa@users.noreply.github.com>
2023-04-07 22:27:28 +0530

    Add files via upload
    new file added

---
+++ Pattern in CPP/Pattern10.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+//first method
+    // int row = 1;
+    // while (row <= n)
+    // {
+    //     int col = 1;
+    //     while (col <= n)
+    //     {
+    //         char ch = ('A' + row - 1);
+    //         cout << ch;
+    //         col += 1;
+    //     }
+    //     cout << endl;
+    //     row += 1;
+    // }
+
+   //Second Method
+    // char cont = 65;
+    // int row = 1;
+    // while (row <= n)
+    // {
+    //     int col = 1;
+
+    //     while (col <= n)
+    //     {
+    //         cout << cont;
+
+    //         col++;
+    //     }
+    //     cont++;
+    //     cout << endl;
+    //     row++;
+    // }
+}
