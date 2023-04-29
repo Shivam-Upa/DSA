e3d7316d83f028593dede7b7c26e4be305353c4e
Shivam Upadhyay <70527646+Shivam-Upa@users.noreply.github.com>
2023-04-29 21:48:20 +0530

    new file added

---
+++ Pattern in CPP/A_test.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    int row = 1;
+    int col = 1;
+
+    while (row <= n)
+    {
+        int space = 0;
+        int star = 0;
+        while (space<col)
+        {
+            cout<<" ";
+            ++space;
+        }
+        while (star<n)
+        {
+            cout<<"*";
+            ++star;
+        }
+        
+        
+        cout << endl;
+        --n;
+        row = row + 1;
+        col = col +1;
+    }
+}
